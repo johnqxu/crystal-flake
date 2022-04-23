a302bc3237a3ed0952b2bccf81eeaeb8651a5bea
Frank Lin <f.lin@sap.com>
2022-04-23 15:44:26 +0800

    worked

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.github.johnqxu.crystalflake{
+    exports com.github.johnqxu.crystalflake;
+    exports com.github.johnqxu.crystalflake.annotation;
+    requires  lombok;
+    requires org.slf4j;
+    requires spring.context;
+    requires spring.beans;
+}
